546ddf39fb8df9e7905579667bf81ac975ce93bd
maxiamikel <maxloversist@gmail.com>
2022-12-26 22:17:13 -0300

    Inicialização e configuração do projeto

---
+++ src/main/java/com/maxi/app_colegio/AppColegioApplication.java
@@ -0,0 +1,13 @@
+package com.maxi.app_colegio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppColegioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppColegioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxi/app_colegio/AppColegioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxi.app_colegio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppColegioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
