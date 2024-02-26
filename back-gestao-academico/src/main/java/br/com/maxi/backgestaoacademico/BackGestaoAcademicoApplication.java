c48625aaa359ea72b18126600de693530fe81a60
maxiamikel <maxloversist@gmail.com>
2024-02-26 14:42:03 -0300

    Initial commit

---
+++ back-gestao-academico/src/main/java/br/com/maxi/backgestaoacademico/BackGestaoAcademicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.maxi.backgestaoacademico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackGestaoAcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackGestaoAcademicoApplication.class, args);
+	}
+
+}
